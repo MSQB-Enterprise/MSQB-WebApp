95e09818f83f8b35dd2883f5f046ec7b2844a14f
jordanrob709 <jordanrob709@gmail.com>
2021-05-07 23:55:58 +0300

    added crispy forms

--- msqb/settings.py
+++ msqb/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #Custom Apps
+    'users.apps.UsersConfig',
+    'blog.apps.BlogConfig',
+    'media.apps.MediaConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
